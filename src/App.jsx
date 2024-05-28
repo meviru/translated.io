e1cf473bf24d8424b1b3101e0048f1e1209216d2
Viral Patel <viral.patel@1rivet.com>
2024-05-28 17:30:45 +0530

    feat: minor ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import iconSwitch from "./assets/Horizontal_top_left_main.svg"
 import iconSpeaker from "./assets/sound_max_fill.svg"
 import iconCopy from "./assets/Copy.svg"
 import iconTranslate from "./assets/Sort_alfa.svg"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useDebounce } from 'use-debounce';
 
 const API_URL = "https://api.mymemory.translated.net";
@@ -15,8 +15,31 @@ function App() {
   const [translateValue] = useDebounce(translate, 500);
   const [translated, setTranslated] = useState("");
 
+  const count = useRef(0);
+
+  const LANGUAGES = {
+    AUTO: 'autodetect',
+    ENGLISH: 'en',
+    FRENCH: 'fr',
+    SPANISH: 'es'
+  }
+
+  const [language, setLanguage] = useState("en");
+  const selectLanguage = (lang) => {
+    setLanguage(lang);
+  }
+
+  const [languageTo, setLanguageTo] = useState("es");
+  const selectLanguageTo = (lang) => {
+    setLanguageTo(lang);
+  }
+
   useEffect(() => {
-    fetch(`${API_URL}/get?q=${translateValue}&langpair=en|fr`)
+    count.current = translate.length;
+  }, [translate])
+
+  useEffect(() => {
+    fetch(`${API_URL}/get?q=${translateValue}&langpair=${language}|${languageTo}`)
       .then(response => response.json())
       .then(data => {
         setTranslated(data.responseData.translatedText);
@@ -25,7 +48,6 @@ function App() {
         console.log(error)
       })
   }, [translateValue])
-
 
   return (
     <>
@@ -38,17 +60,17 @@ function App() {
               <div className={styles.card}>
                 <div className={styles.cardHeader}>
                   <ul className={styles.langSwitch}>
-                    <li className={styles.langSwitchItem}>Delete Language</li>
-                    <li className={`${styles.langSwitchItem} ${styles.active}`}>English</li>
-                    <li className={styles.langSwitchItem}>French</li>
-                    <li className={styles.langSwitchItem}>Spanish</li>
+                    <li onClick={() => selectLanguage('autodetect')} className={`${styles.langSwitchItem} ${language === LANGUAGES.AUTO ? styles.active : ''}`}>Delete Language</li>
+                    <li onClick={() => selectLanguage('en')} className={`${styles.langSwitchItem} ${language === LANGUAGES.ENGLISH ? styles.active : ''}`}>English</li>
+                    <li onClick={() => selectLanguage('fr')} className={`${styles.langSwitchItem} ${language === LANGUAGES.FRENCH ? styles.active : ''}`}>French</li>
+                    <li onClick={() => selectLanguage('es')} className={`${styles.langSwitchItem} ${language === LANGUAGES.SPANISH ? styles.active : ''}`}>Spanish</li>
                   </ul>
                 </div>
                 <div className={styles.cardBody}>
-                  <textarea value={translate} className={styles.inputItem} onChange={(e) => setTranslate(e.target.value)} />
+                  <textarea value={translate} maxLength={500} className={styles.inputItem} onChange={(e) => setTranslate(e.target.value)} />
                 </div>
                 <div className={styles.cardFooter}>
-                  <div className={styles.footerCount}>19/500</div>
+                  <div className={styles.footerCount}>{count.current}/500</div>
                   <div className={styles.footerButtons}>
                     <div className={styles.footerButtonsLeft}>
                       <button type='button' className={styles.iconBtn}>
@@ -68,9 +90,9 @@ function App() {
               <div className={`${styles.card} ${styles.cardAlt}`}>
                 <div className={styles.cardHeader}>
                   <ul className={styles.langSwitch}>
-                    <li className={styles.langSwitchItem}>English</li>
-                    <li className={`${styles.langSwitchItem} ${styles.active}`}>French</li>
-                    <li className={styles.langSwitchItem}>Spanish</li>
+                    <li onClick={() => selectLanguageTo('en')} className={`${styles.langSwitchItem} ${languageTo === LANGUAGES.ENGLISH ? styles.active : ''}`}>English</li>
+                    <li onClick={() => selectLanguageTo('fr')} className={`${styles.langSwitchItem} ${languageTo === LANGUAGES.FRENCH ? styles.active : ''}`}>French</li>
+                    <li onClick={() => selectLanguageTo('es')} className={`${styles.langSwitchItem} ${languageTo === LANGUAGES.SPANISH ? styles.active : ''}`}> Spanish</li>
                   </ul>
                   <div className={styles.cardHeaderRight}>
                     <button type='button' className={styles.iconBtn}>
